e1d4627d98a48f5f2c3e3d889b5a7d85f882c2e5
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-05 18:44:29 -0300

    Wrap teams in a list

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const server = fastify({ logger: true });
 
 server.get("/teams", async (request, response) => {
   response.type("application/json").code(200);
-  return { id: 1, name: "ferrari" };
+  return [{ id: 1, name: "ferrari" }];
 });
 
 server.get("/drivers", async (request, response) => {
