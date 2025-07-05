0131d8508510e4fc576a92116da8225fad9d84f0
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-05 18:49:24 -0300

    Create teams list

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,15 @@ import fastify from "fastify";
 
 const server = fastify({ logger: true });
 
+const teams = [
+  { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
+  { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
+  { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+];
+
 server.get("/teams", async (request, response) => {
   response.type("application/json").code(200);
-  return [{ id: 1, name: "ferrari" }];
+  return teams;
 });
 
 server.get("/drivers", async (request, response) => {
