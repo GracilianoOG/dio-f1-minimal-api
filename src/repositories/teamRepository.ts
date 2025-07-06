ab50de46eac84a2cc916fa50084aa3a42b4cb0dc
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-06 17:08:33 -0300

    Create team repository

---
+++ src/repositories/teamRepository.ts
@@ -0,0 +1,24 @@
+import { TeamModel } from "../models/TeamModel";
+
+const teams: TeamModel[] = [
+  { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
+  { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
+  { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+  { id: 4, name: "Ferrari", base: "Maranello, Italy" },
+  { id: 5, name: "Alpine", base: "Enstone, United Kingdom" },
+  { id: 6, name: "Aston Martin", base: "Silverstone, United Kingdom" },
+  { id: 7, name: "Williams", base: "Grove, United Kingdom" },
+  { id: 8, name: "Alfa Romeo", base: "Hinwil, Switzerland" },
+  { id: 9, name: "Haas F1 Team", base: "Kannapolis, USA" },
+  { id: 10, name: "AlphaTauri", base: "Faenza, Italy" },
+];
+
+export class TeamRepository {
+  async findAllTeams(): Promise<TeamModel[]> {
+    return teams;
+  }
+
+  async findTeamById(id: number): Promise<TeamModel | undefined> {
+    return teams.find(d => d.id === id);
+  }
+}
