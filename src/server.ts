63583e610c07b7477a694b52b5f1b0837301c499
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-06 16:45:33 -0300

    Update teams and drivers

--- src/server.ts
+++ src/server.ts
@@ -13,12 +13,32 @@ const teams = [
   { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
   { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
   { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+  { id: 4, name: "Ferrari", base: "Maranello, Italy" },
+  { id: 5, name: "Alpine", base: "Enstone, United Kingdom" },
+  { id: 6, name: "Aston Martin", base: "Silverstone, United Kingdom" },
+  { id: 7, name: "Williams", base: "Grove, United Kingdom" },
+  { id: 8, name: "Alfa Romeo", base: "Hinwil, Switzerland" },
+  { id: 9, name: "Haas F1 Team", base: "Kannapolis, USA" },
+  { id: 10, name: "AlphaTauri", base: "Faenza, Italy" },
 ];
 
 const drivers = [
   { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
-  { id: 2, name: "Lewis Hamiltom", team: "Ferrari" },
-  { id: 3, name: "Lando Morris", team: "McLaren" },
+  { id: 2, name: "Lewis Hamilton", team: "Mercedes" },
+  { id: 3, name: "Lando Norris", team: "McLaren" },
+  { id: 4, name: "Charles Leclerc", team: "Ferrari" },
+  { id: 5, name: "Fernando Alonso", team: "Aston Martin" },
+  { id: 6, name: "Sebastian Vettel", team: "Aston Martin" },
+  { id: 7, name: "George Russell", team: "Mercedes" },
+  { id: 8, name: "Carlos Sainz", team: "Ferrari" },
+  { id: 9, name: "Pierre Gasly", team: "Alpine" },
+  { id: 10, name: "Kevin Magnussen", team: "Haas F1 Team" },
+  { id: 11, name: "Lance Stroll", team: "Aston Martin" },
+  { id: 12, name: "Yuki Tsunoda", team: "AlphaTauri" },
+  { id: 13, name: "Valtteri Bottas", team: "Alfa Romeo" },
+  { id: 14, name: "Zhou Guanyu", team: "Alfa Romeo" },
+  { id: 15, name: "Alex Albon", team: "Williams" },
+  { id: 16, name: "Nicholas Latifi", team: "Williams" },
 ];
 
 server.get("/teams", async (request, response) => {
