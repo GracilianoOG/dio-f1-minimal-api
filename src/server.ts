720cf63edb440bf4ae5ea85237511608273140b2
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-06 18:11:25 -0300

    Refactor using driver controller
    - Remove drivers array;
    - Retrieve drivers using controller.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import { TeamController } from "./controllers/teamController";
+import { DriverController } from "./controllers/driverController";
 
 const server = fastify({ logger: true });
 
@@ -10,25 +11,6 @@ server.register(cors, {
   origin: "*",
 });
 
-const drivers = [
-  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
-  { id: 2, name: "Lewis Hamilton", team: "Mercedes" },
-  { id: 3, name: "Lando Norris", team: "McLaren" },
-  { id: 4, name: "Charles Leclerc", team: "Ferrari" },
-  { id: 5, name: "Fernando Alonso", team: "Aston Martin" },
-  { id: 6, name: "Sebastian Vettel", team: "Aston Martin" },
-  { id: 7, name: "George Russell", team: "Mercedes" },
-  { id: 8, name: "Carlos Sainz", team: "Ferrari" },
-  { id: 9, name: "Pierre Gasly", team: "Alpine" },
-  { id: 10, name: "Kevin Magnussen", team: "Haas F1 Team" },
-  { id: 11, name: "Lance Stroll", team: "Aston Martin" },
-  { id: 12, name: "Yuki Tsunoda", team: "AlphaTauri" },
-  { id: 13, name: "Valtteri Bottas", team: "Alfa Romeo" },
-  { id: 14, name: "Zhou Guanyu", team: "Alfa Romeo" },
-  { id: 15, name: "Alex Albon", team: "Williams" },
-  { id: 16, name: "Nicholas Latifi", team: "Williams" },
-];
-
 server.get("/teams", async (request, response) => {
   response.type("application/json").code(200);
   return new TeamController().getAllTeams();
@@ -36,7 +18,7 @@ server.get("/teams", async (request, response) => {
 
 server.get("/drivers", async (request, response) => {
   response.type("application/json").code(200);
-  return drivers;
+  return new DriverController().getAllDrivers();
 });
 
 interface DriverParams {
@@ -47,7 +29,8 @@ server.get<{ Params: DriverParams }>(
   "/drivers/:id",
   async (request, response) => {
     const id: number = parseInt(request.params.id);
-    const driver = drivers.find(d => d.id === id);
+    const driverController = new DriverController();
+    const driver = driverController.getDriverById(id);
 
     if (!driver) {
       response.type("application/json").code(404);
