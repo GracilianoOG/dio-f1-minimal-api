618a8a2f2f717e9e5d328a69c0d775fee7f3e622
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-17 23:35:18 -0300

    Import drivers from data

--- src/repositories/driverRepository.ts
+++ src/repositories/driverRepository.ts
@@ -1,23 +1,5 @@
+import { drivers } from "../data/drivers";
 import { DriverModel } from "../models/DriverModel";
-
-const drivers: DriverModel[] = [
-  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
-  { id: 2, name: "Lewis Hamilton", team: "Mercedes" },
-  { id: 3, name: "Lando Norris", team: "McLaren" },
-  { id: 4, name: "Charles Leclerc", team: "Ferrari" },
-  { id: 5, name: "Fernando Alonso", team: "Aston Martin" },
-  { id: 6, name: "Sebastian Vettel", team: "Aston Martin" },
-  { id: 7, name: "George Russell", team: "Mercedes" },
-  { id: 8, name: "Carlos Sainz", team: "Ferrari" },
-  { id: 9, name: "Pierre Gasly", team: "Alpine" },
-  { id: 10, name: "Kevin Magnussen", team: "Haas F1 Team" },
-  { id: 11, name: "Lance Stroll", team: "Aston Martin" },
-  { id: 12, name: "Yuki Tsunoda", team: "AlphaTauri" },
-  { id: 13, name: "Valtteri Bottas", team: "Alfa Romeo" },
-  { id: 14, name: "Zhou Guanyu", team: "Alfa Romeo" },
-  { id: 15, name: "Alex Albon", team: "Williams" },
-  { id: 16, name: "Nicholas Latifi", team: "Williams" },
-];
 
 export const findAllDrivers = async (): Promise<DriverModel[]> => {
   return drivers;
