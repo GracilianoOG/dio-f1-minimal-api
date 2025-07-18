0ea7ef8619cac8f94c114105775819c7edbb4c0d
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-18 01:10:57 -0300

    Create deleteDriver to delete it from db

--- src/repositories/driverRepository.ts
+++ src/repositories/driverRepository.ts
@@ -14,3 +14,14 @@ export const findDriverById = async (
 export const insertDriver = async (driver: DriverModel) => {
   drivers.push(driver);
 };
+
+export const deleteDriver = async (id: number) => {
+  const driverId = drivers.findIndex(driver => driver.id === id);
+
+  if (driverId !== -1) {
+    drivers.splice(driverId, 1);
+    return true;
+  }
+
+  return false;
+};
